lesser-precision-in-norms-bug

HAM/backprop/HAM_backpropagation.py
@@ -1,13 +1,20 @@
+import torch
 from torch import nn
+
+import os
 
 from . import HAMLoss
 from . import NoneOrType
 from collections.abc import Callable
+from typing import Literal
 
 from decimal import Decimal
 
+from tqdm import tqdm
+
 class HAMTrain:
 
+    #MODES = ["causal", "anticausal"]
     MODES = ["causal", "anticausal"]
     SERIALIZATION_DIR = "gradnorms_by_batch/"
 
@@ -28,7 +35,6 @@ class HAMTrain:
                  layers: NoneOrType(list[str]) | NoneOrType(tuple[str]) = None,
                  cpu_gpu_transfer: bool = False,
                  quantifier_fn: Callable[[torch.Tensor], float] = lambda x: x.norm(),
-                 device: torch.device = torch.device("cuda"),
                  variate_names: NoneOrType(list[str] | tuple[str]) = None) -> None:
 
         """
@@ -64,9 +70,14 @@ class HAMTrain:
         self.dataset = dataset_iterator
 
         self.log_folder = log_folder
+        if not os.path.isdir(log_folder):
+            os.makedirs(log_folder)
+
         self.model_name = model_name
         self.horizon_size = horizon_size
+        self.variate_names = variate_names
 
+        self.num_variates = num_variates
         self.variate_indices = variate_indices
         self.layers = layers
 
@@ -87,34 +98,41 @@ class HAMTrain:
                                 for v in variate_indices]
 
         if not layers is None:
-            common_str = os.path.commonprefix(layers)
-            self.filename = lambda x: os.path.join(self.log_folder, "%s%s_%d_gradnorms.txt" % (self.model_name, common_str, self.horizon_size)) \
+            self.filename = lambda x: os.path.join(self.log_folder, "%s%s_%d_gradnorms.txt" % (
+                                        self.model_name, os.path.commonprefix(self.layers), self.horizon_size)) \
                                 if not isinstance(x, int) else \
-                                os.path.join(self.log_folder, "%s%s:%s_%d_gradnorms.txt" % (self.model_name, common_str, variate_names[x], self.horizon_size))
+                                os.path.join(self.log_folder, "%s%s:%s_%d_gradnorms.txt" % (
+                                        self.model_name, os.path.commonprefix(self.layers), self.variate_names[x], self.horizon_size))
         else:
             self.filename = lambda x: os.path.join(self.log_folder, "%s_%d_gradnorms.txt" % (self.model_name, self.horizon_size)) \
                                 if not isinstance(x, int) else \
-                                os.path.join(self.log_folder, "%s:%s_%d_gradnorms.txt" % (self.model_name, variate_names[x], self.horizon_size))
+                                os.path.join(self.log_folder, "%s:%s_%d_gradnorms.txt" % (self.model_name, self.variate_names[x], self.horizon_size))
 
         for mode in self.MODES:
             if os.path.exists(self.filename(mode)):
                 raise FileExistsError("Quantifier files exist for this model: %s!" % self.filename(mode))
 
-        self.layers = layers
-        dims = (len(dataset_iterator), len(layers)) if (num_variates is None or not variate_indices is None) else (len(dataset_iterator), len(layers), len(variate_indices))
+        if num_variates is None:
+            dims = (len(dataset_iterator), len(layers)) 
+        else:
+            if variate_indices is None:
+                dims = (len(dataset_iterator), len(layers), num_variates)
+            else:
+                dims = (len(dataset_iterator), len(layers), len(variate_indices))
 
         if not os.path.isdir(self.SERIALIZATION_DIR):
             os.makedirs(self.SERIALIZATION_DIR)
 
         try:
             self.batch_start = self.deserialize_quantifier_tensor()
+
+        except Exception:
+            self.batch_start = 0
+
             # Keeping dataset's overall tensor in the CPU to enable the same batch sizes as in the training
             self.quantifier_per_timestep = {}
             for mode in self.MODES:
                 self.quantifier_per_timestep[mode] = [torch.zeros(dims) for _ in range(horizon_size+1)]
-
-        except Exception:
-            self.batch_start = 0
 
     def serialize_quantifier_tensor(self, batch_idx: int) -> None:
 
@@ -145,93 +163,96 @@ class HAMTrain:
                 write_str = "Grad norms for subseries %d->%d: " % (start_end[0], start_end[1])
 
                 for idx, layer_name in enumerate(self.model.named_parameters()):
-                    if layer_name in self.layers:
+                    if layer_name[0] in self.layers:
                         if not variate_idx is None:
-                            write_str += "%s=%.16E " % (layer_name, grad_norms_per_timestep[mode][h][:, idx, variate_idx])
+                            write_str += "%s=%.16E " % (layer_name[0], self.quantifier_per_timestep[mode][h][:, idx, variate_idx].mean())
                         else:
-                            write_str += "%s=%.16E " % (layer_name, grad_norms_per_timestep[mode][h][:, idx])
+                            write_str += "%s=%.16E " % (layer_name[0], self.quantifier_per_timestep[mode][h][:, idx].mean())
                 write_str += "\n"
                 f.write(write_str)
 
     def backward(self):
 
         for mode in self.MODES:
+            print (mode)
 
-            for batch_idx, inputs in enumerate(self.dataset):
+            for batch_idx, inputs in enumerate(tqdm(self.dataset)):
 
                 if batch_idx < self.batch_start:
                     continue
 
-                [x.float().to(self.device) for x in inputs]
+                inputs = [x.to(self.device) for x in inputs]
                 input_window, horizon_gt, *metadata = inputs
 
-                horizon_pred = self.model(inputs)
+                horizon_pred = self.model(*inputs)
 
                 for h in range(self.horizon_size + 1):
+                #for h in range(self.horizon_size + 1):
+                    
+                    if h % 50 != 0:
+                        continue
 
                     if any([x in self.model_name for x in self.INTERPOLATION_SUBSERIES_MODELS]):
                         if idx % self.INTERPOLATION_SUBSERIES_LENGTH != 0:
                             continue
 
-                    self.loss_fn.assign_mask(idx, mode)
+                    for loss_idx, loss_fn in enumerate(self.loss_fns):
+                        loss_fn.assign_mask(h, mode)
+                        loss = loss_fn(horizon_pred, horizon_gt)
 
-                    loss = self.loss_fn(horizon_pred, horizon_gt)
+                        # If multivariate or one of the joint distributions of variates
+                        if len(loss.shape) == 0:
 
-                    # If multivariate quantifiers are needed or if a particular distribution of variates is needed
-                    if self.num_variates is None or (not self.num_variates is None and not self.variate_indices is None):
-
-                        loss.backward(retain_graph=True)
-
-                        for idx, (n, param) in enumerate(self.model.named_parameters()):
-                            if not n in self.layers:
-                                continue
-                            if not param.grad is None:
-                                self.quantifier_per_timestep[mode][h][batch_idx][idx] = param.grad.norm()
-
-                        for param in self.model.parameters():
-                            if not param.grad is None:
-                                param.grad.fill_(0)
-
-                    # If independently distributed across variates one by one
-                    else:
-
-                        for v_idx in range(self.variate_indices):
-                            v_loss = loss[v_idx:v_idx+1].mean()
-                            v_loss.backward(retain_graph=True)
+                            loss.backward(retain_graph=True)
 
                             for idx, (n, param) in enumerate(self.model.named_parameters()):
-                                if not n in layer_names:
-                                    print ("CONTINUE")
+                                if not n in self.layers:
                                     continue
                                 if not param.grad is None:
-                                    #grad_norms.append(param.grad.norm())
-                                    grad_norms_per_timestep[mode][h][batch_idx][idx][v_idx] = param.grad.norm()
+                                    if len(self.loss_fns) == 1:
+                                        self.quantifier_per_timestep[mode][h][batch_idx][idx] = param.grad.norm()
+                                    else:
+                                        self.quantifier_per_timestep[mode][h][batch_idx][idx][loss_idx] = param.grad.norm()
 
                             for param in self.model.parameters():
                                 if not param.grad is None:
                                     param.grad.fill_(0)
 
-                        loss = v_loss
+                        # If independently distributed across variates one by one
+                        else:
 
-                save_dict = {"batch": torch.tensor(i), "gradnorms": grad_norms_per_timestep}
-                if not os.path.isdir("gradnorms_temp"):
-                    os.mkdir("gradnorms_temp")
-                torch.save(save_dict, "gradnorms_temp/%s_%d_%s.pth" % (self.args.model, self.args.pred_len, self.args.inspect_backward_pass))
+                            for v_idx in range(self.num_variates):
+                                v_loss = loss[v_idx]
+                                v_loss.backward(retain_graph=True)
+
+                                for idx, (n, param) in enumerate(self.model.named_parameters()):
+                                    if not n in self.layers:
+                                        continue
+                                    if not param.grad is None:
+                                        self.quantifier_per_timestep[mode][h][batch_idx][idx][v_idx] = param.grad.norm()
+
+                                for param in self.model.parameters():
+                                    if not param.grad is None:
+                                        param.grad.fill_(0)
+
+                            loss = v_loss
+
+                self.serialize_quantifier_tensor(batch_idx)
 
                 loss.backward(retain_graph=False)
 
                 # No optim.step() between batches with detach()
                 for param in self.model.parameters():
                     if not param.grad is None:
-                        param.grad.detach()
+                        param.grad.fill_(0).detach()
 
         # Save gradient quantifier to corresponding files!
         for mode in self.MODES:
-            [x.cpu().numpy() for x in grad_norms_per_timestep[mode]]
-            if len(grad_norms_per_timestep[mode][0]) == 3:
-                for v_idx in range(len(self.variate_names)):
+            self.quantifier_per_timestep[mode] = [x.cpu().numpy() for x in self.quantifier_per_timestep[mode]]
+            if len(self.quantifier_per_timestep[mode][0].shape) == 3:
+                for v_idx in range(self.quantifier_per_timestep[mode][0].shape[-1]):
                     fname = self.filename(v_idx)
                     self.write_log_file(fname, mode, v_idx)
-           else:
+            else:
                 fname = self.filename(None)
                 self.write_log_file(fname, mode)

HAM/backprop/HAM_loss.py
@@ -36,11 +36,14 @@ class HAMLoss(nn.Module):
         else:
             raise NotImplementedError
 
+        self.horizon_size = horizon_size
+
         assert variate_indices is None or all([x < num_variates for x in variate_indices]), "Invalid variate dimensions!"
 
         if num_variates is None:
             self.mask = torch.zeros(horizon_size).to(device)
             self.dims = (0, 2)
+            self.working_mask = self.mask.clone()
 
         else:
             assert isinstance(num_variates, int), "Integer size only!"
@@ -49,65 +52,78 @@ class HAMLoss(nn.Module):
                 self.mask = torch.zeros((horizon_size, num_variates, num_variates)).to(device)
                 for idx in range(num_variates):
                     self.mask[:,idx,idx] = 1
+                self.working_mask = self.mask.clone()
             else:
                 self.mask = torch.zeros((horizon_size, num_variates)).to(device)
                 self.variate_indices = torch.tensor(variate_indices).to(device)
+                self.working_mask = self.mask.clone()
 
             self.dims = (0,)
 
         if not cpu_gpu_transfer:
-            if variate_indices is None:
-                self.indices_tensors = [None] + [torch.tensor(range(0, h)).to(device) for h in range(1, horizon_size+1)]
-
-            else:
-                self.indices_tensors = [None] + [torch.tensor(range(0, h)).to(device) for h in range(1, horizon_size+1)]
+            self.indices_tensors = [None] + [torch.tensor(range(0, h)).long().to(device) for h in range(1, horizon_size+1)]
+            self.indices_tensors_rev = [torch.tensor(range(h, horizon_size)).long().to(device) for h in range(0, horizon_size)] + [None]
         else:
             self.device = device
 
         self.cpu_gpu_transfer = cpu_gpu_transfer
 
     def assign_mask(self, timestep: int, mode: Literal["causal", "anticausal"]) -> None:
+        
+        if timestep == 0 and mode == "causal" or timestep == self.horizon_size and mode == "anticausal":
+            self.working_mask.fill_(0)
+        else:
+            self.working_mask.copy_(self.mask)
 
-        self.mask.fill_(0)
+        if len(self.dims) == 2:
+            self.mask.fill_(0)
 
-        if (timestep > 0 and mode == "causal") or \
-                (timestep < self.mask.shape[0] and mode == "anticausal"):
-            if len(self.dims) == 2:
-                if self.cpu_gpu_transfer:
-                    ones_idx = torch.tensor(range(0, timestep)).to(self.device) if mode == "causal" \
-                                    else torch.tensor(range(timestep, self.mask.shape[0])).to(self.device)
-                else:
-                    ones_idx = self.indices_tensors[timestep if mode == "causal" else self.mask.shape[0]-timestep]
-
-                self.mask.index_fill_(0, ones_idx, 1)
+            if self.cpu_gpu_transfer:
+                ones_idx = torch.tensor(range(0, timestep)).to(self.device) if mode == "causal" \
+                                else torch.tensor(range(timestep, self.mask.shape[0])).to(self.device)
             else:
+                ones_idx = self.indices_tensors[timestep if mode == "causal" else self.mask.shape[0]-timestep]
 
-                if len(self.mask.shape) != 3:
-                    self.mask.index_fill_(1, self.variate_indices, 1) 
+            if not ones_idx is None:
+                self.working_mask.index_fill_(0, ones_idx, 1)
+        else:
 
-                if self.cpu_gpu_transfer:
-                    zeroes_idx = torch.tensor(range(0, timestep)).to(self.device) if mode == "anticausal" \
-                                    else torch.tensor(range(timestep, self.mask.shape[0])).to(self.device)
-                else:
-                    zeroes_idx = self.indices_tensors[self.mask.shape[0]-timestep if mode == "causal" else timestep]
-                if not zeroes_idx is None and len(zeroes_idx) > 0:
-                    self.mask.index_fill_(0, zeroes_idx, 0) 
+            if len(self.mask.shape) != 3:
+                self.mask.index_fill_(1, self.variate_indices, 1) 
+
+            if self.cpu_gpu_transfer:
+                zeroes_idx = torch.tensor(range(0, timestep)).to(self.device) if mode == "anticausal" \
+                                else torch.tensor(range(timestep, self.mask.shape[0])).to(self.device)
+            else:
+                zeroes_idx = self.indices_tensors[timestep] if mode == "anticausal" else self.indices_tensors_rev[timestep]
+                if not zeroes_idx is None:
+                    self.working_mask.index_fill_(0, zeroes_idx, 0)
 
     def MSE_per_timestep(self, pred: torch.Tensor, gt: torch.Tensor) -> torch.Tensor:
+        # B x V x H
+
+        # Ignore Transformer model outputs
+        pred = pred[:, :, -self.horizon_size:]
+        gt = gt[:, :, -self.horizon_size:]
 
         se = (pred - gt)**2
         se = se.mean(self.dims)
-        
-        if len(self.mask.shape) == 3:
-            se = torch.einsum("...hv,...hwv->...hw", se, self.mask)
+
+        if len(self.working_mask.shape) == 3:
+            se = torch.einsum("...vh,...hvw->...hw", se, self.working_mask)
             loss = se.mean(dim=0)
         else:
-            se = se * self.mask
+            se = se * self.working_mask
             loss = se.mean()
-        
+
         return loss
 
     def sMAPE_per_timestep(self, pred: torch.Tensor, gt: torch.Tensor) -> torch.Tensor:
+        # B x V x H
+
+        # Ignore Transformer model outputs
+        pred = pred[:, :, -self.horizon_size:]
+        gt = gt[:, :, -self.horizon_size:]
 
         ae = torch.abs(pred - gt)
         factor = torch.abs(pred) + torch.abs(gt)
@@ -117,61 +133,75 @@ class HAMLoss(nn.Module):
         smape[smape != smape] = 0
 
         smape = smape.mean(self.dims)
-        
-        if len(self.mask.shape) == 3:
-            smape = torch.einsum("...hv,...hwv->...hw", smape, self.mask)
+
+        if len(self.working_mask.shape) == 3:
+            smape = torch.einsum("...vh,...hvw->...hw", smape, self.working_mask)
             loss = smape.mean(dim=0)
         else:
-            smape = smape * self.mask
+            smape = smape * self.working_mask
             loss = smape.mean()
 
         return loss
 
     def MAPE_per_timestep(self, pred: torch.Tensor, gt: torch.Tensor) -> torch.Tensor:
+        # B x V x H
 
-        N = pred.shape[1]
+        # Ignore Transformer model outputs
+        pred = pred[:, :, -self.horizon_size:]
+        gt = gt[:, :, -self.horizon_size:]
+
+        N = pred.shape[-1]
         ape = (100. / N) * torch.abs((gt - pred) / gt)
 
         ape = ape.mean(self.dims)
-        
-        if len(self.mask.shape) == 3:
-            ape = torch.einsum("...hv,...hwv->...hw", ape, self.mask)
+
+        if len(self.working_mask.shape) == 3:
+            ape = torch.einsum("...vh,...hvw->...hw", ape, self.working_mask)
             loss = ape.mean(dim=0)
         else:
-            ape = ape * self.mask
+            ape = ape * self.working_mask
             loss = ape.mean()
 
         return loss
 
     def MASE_per_timestep(self, pred: torch.Tensor, gt: torch.Tensor) -> torch.Tensor:
-
+        # B x V x H
         # Forecast window taken as entire dataset for scale for now (similar to instance norm vs norm)
 
-        N = pred.shape[1]
+        # Ignore Transformer model outputs
+        pred = pred[:, :, -self.horizon_size:]
+        gt = gt[:, :, -self.horizon_size:]
+
+        N = pred.shape[-1]
         scale = torch.abs(gt[:, 1:, :] - gt[:, :-1, :]) / (N - 1.)
         ase = torch.abs(pred - gt) / N
 
         ase = ase.mean(self.dims)
-        
-        if len(self.mask.shape) == 3:
-            ase = torch.einsum("...hv,...hwv->...hw", ase, self.mask)
+
+        if len(self.working_mask.shape) == 3:
+            ase = torch.einsum("...vh,...hvw->...hw", ase, self.working_mask)
             loss = ase.mean(dim=0)
         else:
-            ase = ase * self.mask
+            ase = ase * self.working_mask
             loss = ase.mean()
 
         return loss
 
     def MAE_per_timestep(self, pred: torch.Tensor, gt: torch.Tensor) -> torch.Tensor:
+        # B x V x H
+
+        # Ignore Transformer model outputs
+        pred = pred[:, :, -self.horizon_size:]
+        gt = gt[:, :, -self.horizon_size:]
 
         ae = torch.abs(pred - gt)
         ae = ae.mean(self.dims)
-        
-        if len(self.mask.shape) == 3:
-            ae = torch.einsum("...hv,...hwv->...hw", ae, self.mask)
+
+        if len(self.working_mask.shape) == 3:
+            ae = torch.einsum("...vh,...hvw->...hw", ae, self.working_mask)
             loss = ae.mean(dim=0)
         else:
-            ase = ae * self.mask
+            ae = ae * self.working_mask
             loss = ae.mean()
 
         return loss